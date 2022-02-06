1ef4295a52e60308d6afbd11c33b5e5bb74036a8
Dakota Kim <codingvampire@gmail.com>
2022-02-06 12:44:46 -0500

    Initial Commit

---
+++ HackerNewsTV/HackerNewsTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsTVApp.swift
+//  HackerNewsTV
+//
+//  Created by Dakota Kim on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
